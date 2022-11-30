e51cf92f40cc3ef4654211b133548a57f8ab2c5d
DinhHuynhTienPhu <ff10111011@gmail.com>
2022-11-30 12:39:03 +0700

    Revert "Merge branch 'homepage' into feature/members"
    This reverts commit 37702c82cb46d03515305bb145709c18fca6b804, reversing
    changes made to 3018f871c7b9dab7378ba3defd9c3174165564c5.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import { Col, Image, Row, Button } from "antd"
+import React from "react";
+import { Col, Image, Row, Button } from "antd";
 import { Link } from "react-router-dom";
-import '../Home/home.css'
 function Home() {
   const groupList = [
     {
@@ -13,74 +12,98 @@ function Home() {
     {
       image: "This is image",
       nameGroup: "Kien truc phan mem",
-      createdBy: "Dinh Huynh Tien Phu"
-    }
-  ]
+      createdBy: "Dinh Huynh Tien Phu",
+    },
+  ];
   return (
     <div>
       <Row justify="space-between">
-        <Col span={4}>
-        </Col>
+        <Col span={4}></Col>
         <Col span={12}>
-          <section className="bg-white rounded-md my-5 shadow-md">
-            <p className="font-bold text-lg pt-3 ml-5">Group</p>
+          <section className="bg-white rounded-sm">
+            <p className="font-bold ml-2 text-lg pt-2">Group</p>
             <hr />
             <div className="w-4/5 rounded-sm border-2 my-2 m-auto">
               <div className="w-full flex justify-around">
                 <Image src={require("../../assets/images/Kahoot.png")} />
                 <div>
                   <h1>{groupList[0].nameGroup}</h1>
-                  <div><p>{groupList[0].createdBy}</p></div>
-                  <div><p>{groupList[0].createdDate}</p></div>
+                  <div>
+                    <p>{groupList[0].createdBy}</p>
+                  </div>
+                  <div>
+                    <p>{groupList[0].createdDate}</p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="text-center pb-1">
-              <Link><p className="font-bold color-[#1368CE]"><u>See all</u></p></Link>
+            <div className="text-center">
+              <Link>
+                <p className="font-bold color-[#1368CE]">
+                  <u>See all</u>
+                </p>
+              </Link>
             </div>
           </section>
-          <section className="bg-white rounded-md shadow-md">
-            <p className="font-bold ml-5 text-lg pt-3">My kahoots</p>
+          <section className="bg-white rounded-sm">
+            <p className="font-bold ml-2 text-lg pt-2">My kahoots</p>
             <hr />
             <div className="w-4/5 rounded-sm border-2 my-2 m-auto">
               <div className="w-full flex justify-around">
                 <Image src={require("../../assets/images/Kahoot.png")} />
                 <div>
                   <h1>{groupList[0].nameGroup}</h1>
-                  <div><p>{groupList[0].createdBy}</p></div>
-                  <div><p>{groupList[0].createdDate}</p></div>
+                  <div>
+                    <p>{groupList[0].createdBy}</p>
+                  </div>
+                  <div>
+                    <p>{groupList[0].createdDate}</p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="text-center pb-1">
-              <Link><p className="font-bold color-[#1368CE]"><u>See all</u></p></Link>
+            <div className="text-center">
+              <Link>
+                <p className="font-bold color-[#1368CE]">
+                  <u>See all</u>
+                </p>
+              </Link>
             </div>
           </section>
         </Col>
         <Col span={4}>
-          <Button className="mb-4 w-full boder-2 rounded-md bg-[#1260BE]">Host Kahoot</Button>
-          <section className="bg-white rounded-md shadow-md" >
-            <p className="font-bold ml-5 text-lg pt-3">Latest repost</p>
+          <Button className="mb-4 w-full boder-2 rounded-md bg-[#1260BE]">
+            Host Kahoot
+          </Button>
+          <section className="bg-white rounded-sm">
+            <p className="font-bold ml-2 text-lg pt-2">Latest repost</p>
             <hr />
             <div className="w-4/5 rounded-sm border-2 my-2 m-auto">
               <div className="w-full flex justify-around">
                 <Image src={require("../../assets/images/Kahoot.png")} />
                 <div>
                   <h1>{groupList[0].nameGroup}</h1>
-                  <div><p>{groupList[0].createdBy}</p></div>
-                  <div><p>{groupList[0].createdDate}</p></div>
+                  <div>
+                    <p>{groupList[0].createdBy}</p>
+                  </div>
+                  <div>
+                    <p>{groupList[0].createdDate}</p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="text-center pb-1">
-              <Link><p className="font-bold color-[#1368CE]"><u>See all</u></p></Link>
+            <div className="text-center">
+              <Link>
+                <p className="font-bold color-[#1368CE]">
+                  <u>See all</u>
+                </p>
+              </Link>
             </div>
           </section>
         </Col>
       </Row>
     </div>
-
-  )
+  );
 }
 
-export default Home
+export default Home;
