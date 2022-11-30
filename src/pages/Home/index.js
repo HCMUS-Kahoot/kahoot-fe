e5bbadc78688d9102edec38a8be704398018a2fd
DinhHuynhTienPhu <ff10111011@gmail.com>
2022-11-30 16:11:52 +0700

    fix header black space

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Image, Row, Button } from "antd";
 import { Link } from "react-router-dom";
+import '../Home/home.css'
 function Home() {
   const groupList = [
     {
