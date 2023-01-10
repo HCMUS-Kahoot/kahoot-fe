f00aac8487783da14c04d1ec47a686d5ee9f6c09
hatieudao <froyal2kk1@gmail.com>
2023-01-10 14:39:04 +0700

    feat:basical authorize

--- src/pages/presentation/presentationPinInput.js
+++ src/pages/presentation/presentationPinInput.js
@@ -1,17 +1,67 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Button, Col, Input, message } from "antd";
 import "antd/dist/antd.min.css";
-
+import { useSelector } from "react-redux";
+import { Context as RealtimeContext } from "../../store/context/realtimeContext";
 
 export default function PresentationPinInput() {
   const [pin, setPin] = useState("");
   const navigate = useNavigate();
-
+  const { state, initialize_socket, disconnect_socket, check_user_can_join_room } = useContext(RealtimeContext);
+  const user = useSelector((state) => state.auth.login.currentUser);
   useEffect(() => {
+    const handleConnectSocket = async () => {
+      try {
+        const actions = {
+          connected: (data) => console.log("Connected with socket ID: ", data),
+          error: (data) => console.log("Failed to connect socket: ", data),
 
+          error_join_room: (data) => {
+            console.log("event: 'error_join_room' received: ", data)
+            message.error(data.message)
+            navigate("/")
+          },
+          check_user_can_join_room: (data) => {
+            console.log("event: 'join_room_by_pin_success' received: ", data)
+            if (data.status === "OK") {
+              message.success("Join successfully")
+              if (!data.role || data.role === "member") {
+                navigate(`/presentations/${data.pin}/choose`)
+              }
+              else {
+                navigate(`/presentations/${data.pin}/show`)
+              }
+            }
+            else {
+              message.error(data.message)
+            }
+          }
+        }
+        await initialize_socket(actions)
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleConnectSocket();
+
+    return () => {
+      console.log("disconnecting socket")
+      disconnect_socket()
+    }
   }, [])
-
+  const handleSubmitPin = async () => {
+    try {
+      await check_user_can_join_room({
+        userId: user.id,
+        pin: pin,
+      })
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <>
@@ -47,17 +97,7 @@ export default function PresentationPinInput() {
             <br />
             <br />
             <Button className="rounded-md"
-              onClick={
-                () => {
-                  if (pin === "1234") {
-                    message.success("Navigating to presentation...")
-                    navigate(`/presentations/${pin}/choose`)
-                  }
-                  else {
-                    message.error("Pin is incorrect")
-                  }
-                }
-              }
+              onClick={handleSubmitPin}
               style={{
                 marginLeft: '10%',
                 width: '60%',
